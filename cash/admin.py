8265b7e53448f6eac601360721d3077b2cd1edba
zwjtest1 <zwj_12@163.com>
2020-05-31 17:52:31 +0800

    Add modeladmin

--- cash/admin.py
+++ cash/admin.py
@@ -6,5 +6,17 @@ from django.contrib import admin
 from .models import AccountingSubject, CashOnHand
 
 
-admin.site.register(AccountingSubject)
-admin.site.register(CashOnHand)
+class AccountingSubjectAdmin(admin.ModelAdmin):
+    fields = ['accounting_subject', 'debit_balance', 'remark']
+    list_display = ('accounting_subject', 'debit_balance', 'remark')
+
+
+class CashOnHandAdmin(admin.ModelAdmin):
+    fields = ['operation_date', 'serial_number', 'opposite_account', 'summary', 'lucre', 'balance', 'remark']
+    list_display = ('operation_date', 'serial_number', 'opposite_account', 'summary', 'lucre', 'balance', 'remark')
+    list_filter = ['operation_date']
+    search_fields = ['summary']
+
+
+admin.site.register(AccountingSubject, AccountingSubjectAdmin)
+admin.site.register(CashOnHand, CashOnHandAdmin)
